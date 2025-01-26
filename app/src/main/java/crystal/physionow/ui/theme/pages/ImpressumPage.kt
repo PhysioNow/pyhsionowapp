09d45d64916d6ef65f023176c7ba2cd0e1bafab7
Jannik Wappler <RavoxX.wappler123@gmail.com>
2025-01-26 20:51:25 +0100

    - Ich habe die ImpressumPage.kt ausgeschrieben - hotfix

--- app/src/main/java/crystal/physionow/ui/theme/pages/ImpressumPage.kt
+++ app/src/main/java/crystal/physionow/ui/theme/pages/ImpressumPage.kt
@@ -24,7 +24,7 @@ fun ImpressumPage() {
                 contentAlignment = Alignment.TopCenter
             ) {
                 Text(
-                    text = "An Android Application developed by Jannik Wappler \nSternenwinkel 5\n04821 Brandis\nDeutschland\nTel: +49 176 51079224 \n \n \n \n \nGrundidee: \nPhysioNow ist eine App, die Physiotherrappie digitalisieren soll. Es gibt viele Funktionen, wie zum Beispiel, Einen QR-Code Scanner, mit welchem man sein Dokument einscannen kann, und einem direkt passende Übungen angezeigt werden. Außerdem eine Funktion um Gemini nach passenden Übungen zu fragen. \n \n © Copyright by Jannik Wappler \ndarunter Zählt: Idee der App, jegliche Bilder und Struktur",
+                    text = "An Android Application developed by Jannik Wappler \nSternenwinkel 5\n04821 Brandis\nDeutschland\nTel: +49 176 51079224 \n \n \n \n \nGrundidee: \nPhysioNow ist eine App, die Physiotherrappie digitalisieren soll. Es gibt viele Funktionen, wie zum Beispiel, Einen QR-Code Scanner, mit welchem man sein Dokument einscannen kann, und einem direkt passende Übungen angezeigt werden. Außerdem eine Funktion um Gemini nach passenden Übungen zu fragen. \n \n © Copyright by Jannik Wappler \ndarunter Zählt: Idee der App, jegliche Bilder und Struktur. SourceCode unter crystal.physionow ist im geistigen Eigentum von Jannik Wappler",
                     style = MaterialTheme.typography.bodyLarge,
                     modifier = Modifier.padding(16.dp)
                 )
